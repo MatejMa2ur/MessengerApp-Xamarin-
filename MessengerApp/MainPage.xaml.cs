c6f8a6b6e4a5aa444d8ca35aa4b886688716bc56
Matej Mazúr <41269745+MatejMa2ur@users.noreply.github.com>
2021-03-22 08:20:40 +0100

    Add project files.

---
+++ MessengerApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using MessengerApp.Models;
+using System.Net;
+
+namespace MessengerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "https://xamarinfinal.azurewebsites.net/api/Messages";
+        private readonly HttpClient _client = new HttpClient();
+        private IEnumerable<Message> _messages;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Send.Clicked += async (sender, e) =>
+            {
+                string url = $"https://xamarinfinal.azurewebsites.net/api/SendMessage?name={meno.Text}&message={text.Text}";
+                await _client.PostAsync(url,null);
+                MyListView.ItemsSource = null;
+                OnAppearing();
+                text.Text = null;
+            };
+        }
+
+        protected override async void OnAppearing()
+        {
+            string content = await _client.GetStringAsync(Url);
+            IEnumerable<Message> Messages = JsonConvert.DeserializeObject<IEnumerable<Message>>(content);
+            //_messages = new IEnumerable<Message>(Messages);
+            MyListView.ItemsSource = Messages;
+            base.OnAppearing();
+        }
+
+    }
+}
---
+++ MessengerApp/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessengerApp.Models
+{
+    class Message
+    {
+        public Guid id { get; set; }
+        public string title { get; set; }
+        public string name { get; set; }
+        public string message { get; set; }
+    }
+}
